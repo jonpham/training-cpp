38425bc0c560324ecb123c7af0f98350beebffad
Jonathan Pham <jonpham247@gmail.com>
2016-06-27 01:13:13 -0700

    Add Job Variables to Build Script.

--- build.sh
+++ build.sh
@@ -4,11 +4,12 @@
 pushd . > /dev/null
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+NUMCPUS=`grep -c '^processor' /proc/cpuinfo`
 
 mkdir -p $SCRIPT_DIR/build/;
 cd $SCRIPT_DIR/build/;
 
 cmake3 ..
-make
+time nice make -j$NUMCPUS --load-average=$NUMCPUS
 
 popd > /dev/null
