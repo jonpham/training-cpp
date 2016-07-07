561f1051f53294fe7d050173616ce610eab5b15d
Jonathan Pham <jonathan.pham@baesystems.com>
2016-07-07 15:28:34 -0700

    PSTRNG-14 : Add Menu Class Object and CommandLineInterface Constructor with Menu parameter.

--- src/cli_library/CliMenu.cpp
+++ src/cli_library/CliMenu.cpp
@@ -2,17 +2,20 @@
 // Created by Jonathan Pham on 6/21/16.
 //
 
-// #include "sample_static_lib/static_classes.h"
-// #include "sample_shared_lib/shared_classes.h"
+#include "CliMenu.h"
 
+int Menu::loadMenuItems() {
+    // std::shared_ptr<IMenuItem> menu_item1(new SayHello);
+    // this->registerMenuItem(menu_item1);
+    return 1;
+}
 
-// int Menu::registerMenuItem(std::shared_ptr<IMenuItem>& menu_item) {
-//     m_menuList.push_back(menu_item);
-//     return 1;
-// }
+int Menu::registerMenuItem(std::shared_ptr<IMenuItem>& menu_item) {
+    m_menuList.push_back(menu_item);
+    return 1;
+}
 
-// int Menu::initializeMenu() {
-//     std::shared_ptr<IMenuItem> menu_item1(new SayHello);
-//     this->registerMenuItem(menu_item1);
-//     return 1;
-// }
+void Menu::displayUsageText(){
+	std::cout << "\nDisplaying Menu Usage Text." << std::endl;
+	return;
+}
