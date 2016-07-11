367dd1e46ad524bf5becfae66247a02088e071de
Jonathan Pham <jonathan.pham@baesystems.com>
2016-07-11 12:43:09 -0700

    PSTRNG-21 : Add Validation step to check for upper bounds of Data Set size.

--- src/mpp_library/MaxPairProduct.cpp
+++ src/mpp_library/MaxPairProduct.cpp
@@ -6,13 +6,44 @@
 int MaxPairProduct::calculate(const std::vector<int>& data_set)
 {
   int product_result = 0;
-  int n = data_set.size();
-  for (int i = 0; i < n; ++i) {
-    for (int j = i + 1; j < n; ++j) {
-      if (data_set[i] * data_set[j] > product_result) {
-        product_result = data_set[i] * data_set[j];
+  if ( validMinMaxDataInput(data_set) && validDataSetSize(data_set) )
+  {
+    // Algorithm
+    int n = data_set.size();
+    for (int i = 0; i < n; ++i) {
+      for (int j = i + 1; j < n; ++j) {
+        if (data_set[i] * data_set[j] > product_result) {
+          product_result = data_set[i] * data_set[j];
+        }
       }
     }
   }
+  else 
+  {
+    std::string error_msg = "Either the MinMaxDataInput or Data Set Size was outside of constraints.";
+    throw std::out_of_range(error_msg);
+  }
+
   return product_result;
+}
+
+bool MaxPairProduct::validMinMaxDataInput(const std::vector<int>& data_set)
+{
+  int max_pt{2};
+  int min_pt{200000}; 
+  for ( int i = 0; i < data_set.size() ; ++i){
+    break;
+  }
+  return true;
+}
+
+bool MaxPairProduct::validDataSetSize(const std::vector<int>& data_set)
+{
+  int max_n=100000;
+  if (data_set.size() > max_n){
+    return false;
+  }
+  else{
+    return true;
+  }
 }
