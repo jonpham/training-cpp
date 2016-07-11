a4cb42a61ff2a590bc809a145559da1cc589dc3f
Jonathan Pham <jon.pham@gmail.com>
2016-07-11 08:50:51 -0700

    PSTRNG-18 : Add Initial MaxPaired Files and integrated into build.

---
+++ src/mpp_library/MaxPairProduct.cpp
@@ -0,0 +1,6 @@
+// Behavior Implementation for MaxPairProduct Module
+// Created by Jonathan Pham on 6/21/16.
+
+#include "MaxPairProduct.h"
+
+
--- src/mpp_library/shared_classes.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Jonathan Pham on 6/21/16.
-//
-
-// #include "sample_shared_lib/shared_classes.h"
-
-// void SayHello::showDescription() {
-//     std::cout << "Hello, World!" << std::endl;
-// }
