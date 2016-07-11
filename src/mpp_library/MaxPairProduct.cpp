44cc97a43121cc0855d22949682b5c9b3cd77f8a
Jonathan Pham <jon.pham@gmail.com>
2016-07-11 12:07:04 -0700

    PSTRNG-22 : Add Initial Naive Algorithm for Maximum Pair Product.

--- src/mpp_library/MaxPairProduct.cpp
+++ src/mpp_library/MaxPairProduct.cpp
@@ -3,4 +3,16 @@
 
 #include "MaxPairProduct.h"
 
-
+int MaxPairProduct::calculate(const std::vector<int>& data_set)
+{
+  int product_result = 0;
+  int n = data_set.size();
+  for (int i = 0; i < n; ++i) {
+    for (int j = i + 1; j < n; ++j) {
+      if (data_set[i] * data_set[j] > product_result) {
+        product_result = data_set[i] * data_set[j];
+      }
+    }
+  }
+  return product_result;
+}
