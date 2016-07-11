3684bb4b9dfed0c3748ae11f84e471c8b416410c
Jonathan Pham <jonathan.pham@baesystems.com>
2016-07-11 12:28:34 -0700

    PSTRNG-21 : Add Test for Upper Bound Test of Data Set Size.

--- test/unit/TestMaxPairedProduct.cpp
+++ test/unit/TestMaxPairedProduct.cpp
@@ -4,6 +4,7 @@
 
 #include <stdlib.h>
 #include <vector>
+#include <stdexcept>
 
 #include "TestMaxPairedProduct.h"
 
@@ -13,16 +14,9 @@ using ::testing::Return;
 TEST_F(TestMaxPairedProduct,lowNumbers)
 {
   // Setup Test Data
-  // int max_num_data_pnts = 10;
-  // int max_data_range = 20;
-  // int n = std::rand() % max_num_data_pnts + 2;
-  // std::vector<int> test_data;
-  // for (int i = 0; i < n; ++i) {
-  //   test_data.push_back(rand() % max_data_range);
-  // }
 
   std::vector<int> test_data = {10, 4, 5, 19, 14};
-  int answer = 19*14;
+  long long answer = 19*14;
   
   // Test // Verify
   ASSERT_EQ(answer,uut_mppCalculator.calculate(test_data));
@@ -31,13 +25,40 @@ TEST_F(TestMaxPairedProduct,lowNumbers)
 TEST_F(TestMaxPairedProduct,highNumbers)
 {
   // Setup Test Data
-  int max_data_range = 100000;
+  int max_data_range = 200000;
   
   std::vector<int> test_data = {10, 4, 5, max_data_range-10, max_data_range};
-  int answer = (max_data_range-10)*max_data_range;
-  
+  long long answer = (max_data_range-10)*max_data_range;
+  int result = uut_mppCalculator.calculate(test_data);
   // Test // Verify
-  ASSERT_EQ(answer,uut_mppCalculator.calculate(test_data));
+  ASSERT_NE(410065408,result) << "You've hit an integer overflow issue.";
+  ASSERT_EQ(answer,result);
+}
+
+TEST_F(TestMaxPairedProduct,highDuplicateNumbers)
+{
+  // Setup Test Data
+  int max_data_range = 200000;
+  
+  std::vector<int> test_data = {10, 4, 5, max_data_range, max_data_range};
+  long long answer = max_data_range*max_data_range;
+  int result = uut_mppCalculator.calculate(test_data);
+  // Test // Verify
+  ASSERT_NE(410065408,result) << "You've hit an integer-type overflow issue.";
+  ASSERT_EQ(answer,result);
+}
+
+TEST_F(TestMaxPairedProduct,ceilingOnNSizeDataset){
+  // Setup Test Data
+  int max_num_data_pnts = 100000; // 10^5
+  int max_data_range = 20;
+  // int n = std::rand() % max_num_data_pnts + 2;
+  std::vector<int> test_data;
+  for (int i = 0; i < max_num_data_pnts+1; ++i) {
+    test_data.push_back(rand() % max_data_range);
+  }
+
+  ASSERT_THROW(uut_mppCalculator.calculate(test_data),std::out_of_range) << "N is Greater than Allowed.";
 }
 
 // Use Google Mock to Fake User Input. 
