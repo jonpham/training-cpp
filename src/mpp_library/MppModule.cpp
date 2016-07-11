9321f080d2e9b55bce1c68f0ddc1205a93700c3a
Jonathan Pham <jon.pham@gmail.com>
2016-07-11 11:00:16 -0700

    PSTRNG-18 : PSTRNG-19. Add User Input methods to MppModule::requestDataSet()

--- src/mpp_library/MppModule.cpp
+++ src/mpp_library/MppModule.cpp
@@ -4,8 +4,20 @@
 
 #include "MppModule.h"
 
-MppModule::MppModule()
-{};
+MppModule::MppModule() :
+ up_inputBehavior(new MenuItemInput())
+{
+  this->setInputBehavior(up_inputBehavior.get());
+  // Setup Usage / Introductions
+  m_menuName="Maximum Product Pair Algorithm Module";
+  std::stringstream description_strm;
+  description_strm  << "This component of the CLI C++ Training Application" << std::endl
+                    << "serves as an example for the BAE P&S Software "     << std::endl
+                    << "Development Workflow and involves a simple algorithm" << std::endl
+                    << "implementation extending the C++ Command Line Interface." << std::endl;
+
+  m_menuDescription = description_strm.str();
+};
 
 void MppModule::setInputBehavior(InputBehavior* input_behavior){
   p_inputBehavior=input_behavior;
@@ -17,11 +29,36 @@ std::string MppModule::showDescription(){
 
 // "Main Method"
 void MppModule::executeModule(){
+  std::cout << m_menuName << std::endl;
+  std::cout << "\n" << showDescription() << "\n";
+
+  this->requestDataSet();
+  std::cout << "The Maximum Pairwise product is : " <<
+    m_maxPairedProductCalculator.calculate(getDataSet()) << std::endl;
   return;
 }
 
 // User Input Method
 std::vector<int> MppModule::requestDataSet(){
+  int set_size{-1};
+  // Convert 'user_input' to int if possible
+  std::cout << "How large is the data set you wish to input [2 -> 2*10^5]? " ;
+  while (set_size==-1) // Loop while Set Size Unset
+  {
+    try {
+      set_size = std::stoi(p_inputBehavior->requestInput());
+    } catch (const std::invalid_argument& exception) {
+      std::cout << "There was a problem processing your input. Please Try again. Error : " << exception.what() << '\n';
+    }
+  }
+  m_dataSize = set_size; // Set Member Variable
+  
+  std::cout << "Input your data set with only space separators.\n> ";
+  std::vector<int> data_set(set_size);
+  for (int i = 0; i < set_size; ++i) {
+    std::cin >> data_set[i];
+  } 
+  m_dataSet = data_set;
   return m_dataSet;
   // Ask for Data. 
 }
